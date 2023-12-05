639a86c77f28d6d50a63e0315bd53d700cfe6e95
Skipper_108 <36970853+LeonardoSforza@users.noreply.github.com>
2023-12-05 11:30:26 +0100

    complete day 2

--- challengeOne.py
+++ challengeOne.py
@@ -15,132 +15,128 @@ for seed in data[0].split(":")[1].strip().split(" "):
 i = 1
 while i < len(data):
     if data[i].split(":")[0] == "seed-to-soil map":
-        print("Doing 1...")
         i += 1
         while data[i] != "":
             numbers = data[i].strip().split(" ")
             soilNum = int(numbers[0])
             seedNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                if seedNum+j in seeds:
-                    mapOfSeeds[seedNum+j] = [soilNum+j]
+            for key, value in mapOfSeeds.items():
+                if (int(numbers[1]) + thirdNum) - key >= 0 and int(numbers[1]) <= key:
+                    mapOfSeeds[key] = [(key - int(numbers[1])) + int(numbers[0])]
             i += 1
         
     
     if data[i].split(":")[0] == "soil-to-fertilizer map":
-        print("Doing 2...")
         i += 1
         while data[i] != "":
             numbers = data[i].strip().split(" ")
             fertNum = int(numbers[0])
             soilNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                for key, value in mapOfSeeds.items():
-                    if value[0] == soilNum+j:
-                        mapOfSeeds[key] = [value[0], fertNum+j]
-            # Check for empty
             for key, value in mapOfSeeds.items():
-                fertNum = value[0]
-                if len(value) == 1:
-                    mapOfSeeds[key] = [value[0], value[0]]
+                if len(value) == 2:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[0] >= 0 and int(numbers[1]) <= value[0]:
+                    mapOfSeeds[key] = [value[0], (value[0] - int(numbers[1])) + int(numbers[0])]
             i += 1
-
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            fertNum = value[0]
+            if len(value) == 1:
+                mapOfSeeds[key] = [value[0], value[0]]
+            
     if data[i].split(":")[0] == "fertilizer-to-water map":
-        print("Doing 3...")
         i += 1
         while data[i] != "":
             numbers = data[i].strip().split(" ")
             waterNum = int(numbers[0])
             fertNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                for key, value in mapOfSeeds.items():
-                    if value[1] == fertNum+j:
-                        mapOfSeeds[key] = [value[0], value[1], waterNum+j]
-            # Check for empty
             for key, value in mapOfSeeds.items():
-                waterNum = value[1]
-                if len(value) == 2:
-                    mapOfSeeds[key] = [value[0], value[1], value[1]]
+                if len(value) == 3:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[1] >= 0 and int(numbers[1]) <= value[1]:
+                    mapOfSeeds[key] = [value[0], value[1], (value[1] - int(numbers[1])) + int(numbers[0])]
             i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            if len(value) == 2:
+                mapOfSeeds[key] = [value[0], value[1], value[1]]
 
     if data[i].split(":")[0] == "water-to-light map":
-        print("Doing 4...")
         i += 1
         while data[i] != "":
             numbers = data[i].strip().split(" ")
             lightNum = int(numbers[0])
             waterNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                for key, value in mapOfSeeds.items():
-                    if value[2] == waterNum+j:
-                        mapOfSeeds[key] = [value[0], value[1], value[2], lightNum+j]
-            # Check for empty
             for key, value in mapOfSeeds.items():
-                waterNum = value[2]
-                if len(value) == 3:
-                    mapOfSeeds[key] = [value[0], value[1], value[2], value[2]]
+                if len(value) == 4:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[2] >= 0 and int(numbers[1]) <= value[2]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], (value[2] - int(numbers[1])) + int(numbers[0])]
             i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            if len(value) == 3:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[2]]
 
     if data[i].split(":")[0] == "light-to-temperature map":
-        print("Doing 5...")
         i += 1
         while data[i] != "":
             numbers = data[i].strip().split(" ")
             tempNum = int(numbers[0])
             lightNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                for key, value in mapOfSeeds.items():
-                    if value[3] == lightNum+j:
-                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], tempNum+j]
-            # Check for empty
             for key, value in mapOfSeeds.items():
-                waterNum = value[3]
-                if len(value) == 4:
-                    mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[3]]
+                if len(value) == 5:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[3] >= 0 and int(numbers[1]) <= value[3]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], (value[3] - int(numbers[1])) + int(numbers[0])]
             i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            if len(value) == 4:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[3]]
 
     if data[i].split(":")[0] == "temperature-to-humidity map":
-        print("Doing 6...")
         i += 1
         while data[i] != "":
             numbers = data[i].strip().split(" ")
             humNum = int(numbers[0])
             tempNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                for key, value in mapOfSeeds.items():
-                    if value[3] == tempNum+j:
-                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], humNum+j]
-            # Check for empty
             for key, value in mapOfSeeds.items():
-                waterNum = value[4]
-                if len(value) == 5:
-                    mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[4]]
+                if len(value) == 6:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[4] >= 0 and int(numbers[1]) <= value[4]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], (value[4] - int(numbers[1])) + int(numbers[0])]
             i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            waterNum = value[4]
+            if len(value) == 5:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[4]]
 
     if data[i].split(":")[0] == "humidity-to-location map":
-        print("Doing 7...")
         i += 1
         while i < len(data):
             numbers = data[i].strip().split(" ")
             locNum = int(numbers[0])
             humNum = int(numbers[1])
             thirdNum = int(numbers[2])
-            for j in range(0, thirdNum):
-                for key, value in mapOfSeeds.items():
-                    if value[3] == humNum+j:
-                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[5], locNum+j]
-            # Check for empty
             for key, value in mapOfSeeds.items():
-                waterNum = value[5]
-                if len(value) == 6:
-                    mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[5], value[5]]
+                if len(value) == 7:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[5] >= 0 and int(numbers[1]) <= value[5]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[5], (value[5] - int(numbers[1])) + int(numbers[0])]
             i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            waterNum = value[5]
+            if len(value) == 6:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[5], value[5]]
     i += 1
 
 minLocation = None
--- challengeTwo.py
+++ challengeTwo.py
@@ -4,32 +4,164 @@ file = open('data.txt', 'r')
 data = file.read()
 data = data.split("\n")
 
-mapOfCards = defaultdict(list)
+mapOfSeeds = defaultdict(list)
+seeds = []
 
-totalNumberOfScratchCards = 0
-cardNum = 1
+seedNumSplit = data[0].split(":")[1].strip().split(" ")
+i = 0
 
-# First Populate mapOfCards - Takes a few min
-for card in data:
-    winningNumbers = card.split("|")[0].split(":")[1].strip().split(" ")
-    yourNumbers = card.split("|")[1].strip().split(" ")
-    mapOfCards[cardNum] = [1, winningNumbers, yourNumbers]
-    cardNum += 1
+print("Starting Seeds")
 
-for key, value in mapOfCards.items():
-    print(value[0])
-    totalNumberOfScratchCards += value[0]
-    if value[0] > 0:
-        for j in range(1, value[0] + 1):
-            cardPoints = 0
-            for num in value[2]:
-                if num == "":
+while i < len(seedNumSplit):
+    if i % 2 == 0: # Even
+        print("Even Num")
+        startNum = int(seedNumSplit[i])
+        for j in range(i, int(seedNumSplit[i+1])):
+            seeds.append(int(startNum + j))
+            mapOfSeeds[int(startNum + j)] = [int(startNum + j)]
+            j +=1
+    i += 1
+
+print("Seeds done")
+
+i = 1
+while i < len(data):
+    if data[i].split(":")[0] == "seed-to-soil map":
+        print("Doing 1")
+        i += 1
+        while data[i] != "":
+            numbers = data[i].strip().split(" ")
+            soilNum = int(numbers[0])
+            seedNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if (int(numbers[1]) + thirdNum) - key >= 0 and int(numbers[1]) <= key:
+                    mapOfSeeds[key] = [(key - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        
+    
+    if data[i].split(":")[0] == "soil-to-fertilizer map":
+        print("Doing 2")
+        i += 1
+        while data[i] != "":
+            numbers = data[i].strip().split(" ")
+            fertNum = int(numbers[0])
+            soilNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if len(value) == 2:
                     continue
-                if num in value[1]:
-                    cardPoints += 1
-            if cardPoints > 0:
-                for i in range(1, cardPoints + 1):
-                    newValue = mapOfCards[key+i][0] + 1
-                    mapOfCards[key+i] = [newValue, mapOfCards[key+i][1], mapOfCards[key+i][2]]
+                if (int(numbers[1]) + thirdNum) - value[0] >= 0 and int(numbers[1]) <= value[0]:
+                    mapOfSeeds[key] = [value[0], (value[0] - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            fertNum = value[0]
+            if len(value) == 1:
+                mapOfSeeds[key] = [value[0], value[0]]
+            
+    if data[i].split(":")[0] == "fertilizer-to-water map":
+        print("Doing 3")
+        i += 1
+        while data[i] != "":
+            numbers = data[i].strip().split(" ")
+            waterNum = int(numbers[0])
+            fertNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if len(value) == 3:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[1] >= 0 and int(numbers[1]) <= value[1]:
+                    mapOfSeeds[key] = [value[0], value[1], (value[1] - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            if len(value) == 2:
+                mapOfSeeds[key] = [value[0], value[1], value[1]]
 
-print(totalNumberOfScratchCards)
+    if data[i].split(":")[0] == "water-to-light map":
+        print("Doing 4")
+        i += 1
+        while data[i] != "":
+            numbers = data[i].strip().split(" ")
+            lightNum = int(numbers[0])
+            waterNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if len(value) == 4:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[2] >= 0 and int(numbers[1]) <= value[2]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], (value[2] - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            if len(value) == 3:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[2]]
+
+    if data[i].split(":")[0] == "light-to-temperature map":
+        print("Doing 5")
+        i += 1
+        while data[i] != "":
+            numbers = data[i].strip().split(" ")
+            tempNum = int(numbers[0])
+            lightNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if len(value) == 5:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[3] >= 0 and int(numbers[1]) <= value[3]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], (value[3] - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            if len(value) == 4:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[3]]
+
+    if data[i].split(":")[0] == "temperature-to-humidity map":
+        print("Doing 6")
+        i += 1
+        while data[i] != "":
+            numbers = data[i].strip().split(" ")
+            humNum = int(numbers[0])
+            tempNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if len(value) == 6:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[4] >= 0 and int(numbers[1]) <= value[4]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], (value[4] - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            waterNum = value[4]
+            if len(value) == 5:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[4]]
+
+    if data[i].split(":")[0] == "humidity-to-location map":
+        print("Doing 7")
+        i += 1
+        while i < len(data):
+            numbers = data[i].strip().split(" ")
+            locNum = int(numbers[0])
+            humNum = int(numbers[1])
+            thirdNum = int(numbers[2])
+            for key, value in mapOfSeeds.items():
+                if len(value) == 7:
+                    continue
+                if (int(numbers[1]) + thirdNum) - value[5] >= 0 and int(numbers[1]) <= value[5]:
+                        mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[5], (value[5] - int(numbers[1])) + int(numbers[0])]
+            i += 1
+        # Check for empty
+        for key, value in mapOfSeeds.items():
+            waterNum = value[5]
+            if len(value) == 6:
+                mapOfSeeds[key] = [value[0], value[1], value[2], value[3], value[4], value[5], value[5]]
+    i += 1
+
+minLocation = None
+for key, value in mapOfSeeds.items():
+    if minLocation == None:
+        minLocation = mapOfSeeds[key][-1]
+    if mapOfSeeds[key][-1] < minLocation:
+        minLocation = mapOfSeeds[key][-1]
+print(minLocation)
